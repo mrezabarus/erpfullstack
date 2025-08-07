20226d8a6175dfc494656cf1a8ffb1aeddf606bf
mrezabarus <mrezabarus87@gmail.com>
2025-08-08 00:33:18 +0700

    fix get to access token

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function HomePage(){
-  const cookieStore = cookies();
-  const token = cookieStore.get('access_token');
+export default async function HomePage(){
+  const cookieStore = await cookies();
+  const token = cookieStore.getAll();
 
   if(!token){
     redirect('/login');
