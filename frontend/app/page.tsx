8c0dd6c343a1ef920ca064a4000aa6375fec7a4c
mrezabarus <mrezabarus87@gmail.com>
2025-08-08 01:10:20 +0700

    deploy ke render

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function HomePage(){
+export default function HomePage(){
   const cookieStore = cookies();
   const token = cookieStore.get('access_token');
 
