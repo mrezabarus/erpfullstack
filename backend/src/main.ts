5a6c11806e50b4e16f2e01a2fc0df04c3512fa10
mrezabarus <mrezabarus87@gmail.com>
2025-08-24 00:52:24 +0700

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
-    cors: {
-      origin: "https://erpfrontend-fawn.vercel.app", 
-      credentials: true,
-    },
+  const app = await NestFactory.create(AppModule);
+
+  // ✅ Whitelist origin
+  const allowedOrigins = [
+    'http://localhost:3000',                 // local dev
+    'https://erpfrontend-fawn.vercel.app',   // frontend Vercel
+  ];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
   });
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
